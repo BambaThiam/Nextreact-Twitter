f35556409674fd6b1bc028aacb4ce0d6e4deac15
DayOneAI <bambathiam88@gmail.com>
2024-03-27 23:00:00 +0100

    feat: ✨Generic fetch #4

--- pages/exercices/1.tsx
+++ pages/exercices/1.tsx
@@ -26,29 +26,96 @@ const TweetsSchema = z.object({
         avatarUrl: z.string().url(),
       }),
       // likes: z.array(z.string()),
+      liked: z.boolean(),
       _count: z.object({
         likes: z.number(),
         replies: z.number(),
       }),
-      liked: z.boolean(),
+      // liked: z.boolean(),
     })
   ),
 });
 
 // const TweetsSchema = z.array(TweetSchema);
 
-// Tu peux `console.log` la réponse de l'API pour voir la structure attendue
-// Tu pourrais utiliser zod transform pour modifier directement dans le schéma la date
-// 💡 const TweetsSchema = z.object({...
-
-const getTweets = async (signal: AbortSignal) => {
-  const res = await fetch('/api/tweets', { signal });
-  const json = await res.json();
-  if (TweetsSchema.safeParse(json).success === false) {
-    throw new Error('❌ Could not fetch tweets');
-  }
-  return TweetsSchema.parse(json);
+// Dans ClientConfig on récupère le params T
+export type ClientConfig<T> = {
+  data?: unknown;
+  // On utilise T dans le zod schema
+  // Ce qui va faire que notre fetch va être automatiquement être typé en fonction du schéma
+  zodSchema?: z.ZodSchema<T>;
+  method?: 'DELETE' | 'GET' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT';
+  headers?: HeadersInit;
+  // Pour pouvoir override la config
+  customConfig?: RequestInit;
+  signal?: AbortSignal;
 };
+
+// On utilise un générique ici pour automatiquement typer le retour de la fonction
+export async function client<T>(
+  url: string,
+  {
+    data,
+    zodSchema,
+    method,
+    headers: customHeaders,
+    signal,
+    customConfig,
+  }: ClientConfig<T> = {} // On passe T en paramètre de ClientConfig
+): Promise<T> {
+  // On retourne Promise<T> pour que le type soit automatiquement déduit
+  const config: RequestInit = {
+    // S'il n'y a pas de method on utilise POST s'il y a des données et GET sinon
+    method: method ?? (data ? 'POST' : 'GET'),
+    // On stringify les données s'il y en a
+    body: data ? JSON.stringify(data) : null,
+    headers: {
+      'Content-Type': data ? 'application/json' : '',
+      Accept: 'application/json',
+      // Mais on laisse l'utilisateur override les headers
+      ...customHeaders,
+    },
+    signal,
+    // On laisse l'utilisateur override la config
+    // S'il passe body, method, headers, etc... on les écrasera
+    ...customConfig,
+  };
+
+  return window.fetch(url, config).then(async (response) => {
+    // on gère le status 401 en arrêtant directement la request
+    if (response.status === 401) {
+      return Promise.reject(new Error("You're not authenticated"));
+    }
+
+    let result = null;
+    // 🦁 à toi de parse le json dans un try catch
+    try {
+      result = response.status === 204 ? null : await response.json();
+    } catch (error: unknown) {
+      return Promise.reject(error);
+    }
+    if (response.ok) {
+      // 🦁 s'il y a un `zodSchema`, on parse `result` sinon on retourne `result`
+      return zodSchema && result ? zodSchema.parse(result) : result;
+    } else {
+      // 🦁 on reject la promesse avec le `result`
+      return Promise.reject(result);
+    }
+  });
+}
+
+const getTweets = async (signal?: AbortSignal) => 
+client(`/api/tweets`, {
+   signal, zodSchema: TweetsSchema 
+  });
+// {
+//   // const res = await fetch('/api/tweets', { signal });
+//   // const json = await res.json();
+//   // if (TweetsSchema.safeParse(json).success === false) {
+//   //   throw new Error('❌ Could not fetch tweets');
+//   // }
+//   // return TweetsSchema.parse(json);
+// };
 export default function FetchAllTweets() {
   const [tweets, setTweets] = useState<TlTweets | null>(null);
   // const [tweets, setTweets] = useState<TlTweet | null>(null);
